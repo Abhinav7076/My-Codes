83b703269ba981a09c0a7c0d8e35063245776cad
abhinav7076 <180010001@iitdh.ac.in>
2021-09-02 09:59:29 +0530

    added

--- Dynamic Programming/Longest Common Subsequence.cpp
+++ Dynamic Programming/Longest Common Subsequence.cpp
@@ -1,22 +1,21 @@
-class Solution {
-	public:
-		int LongestRepeatingSubsequence(string str){
-		    // Code here
-		    int dp[str.length()+1][str.length()+1];
-		    
-		    for(int i=str.length();i>=0;i--){
-		        for(int j=str.length();j>=0;j--){
-		            if(i==str.length() or j==str.length())
-		            dp[i][j]=0;
-		            else if(str[i]==str[j] and i!=j)
-		            dp[i][j]=dp[i+1][j+1] + 1;
-		            else
-		            dp[i][j]=max(dp[i+1][j], dp[i][j+1]);
-		        }
-		    }
-		    
-		    return dp[0][0];
-		    
-		}
-
+class Solution
+{
+    public:
+    //Function to find the length of longest common subsequence in two strings.
+    int lcs(int n, int m, string s1, string s2)
+    {
+        // your code here
+        int dp[n+1][m+1];
+        for(int i=n;i>=0;i--){
+            for(int j=m;j>=0;j--){
+                if(i==n or j==m)
+                dp[i][j]=0;
+                else if(s1[i]==s2[j])
+                dp[i][j] = dp[i+1][j+1] + 1;
+                else
+                dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+            }
+        }
+        return dp[0][0];
+    }
 };
